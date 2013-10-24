decc5e973a94723c8b4eb2b618fe2cb166ae9527
polds <polds@kyanicorp.com>
2013-10-24 17:49:20 -0600

    Better comments

--- images.go
+++ images.go
@@ -25,6 +25,8 @@ func SetDefaultImage(img string) {
 	df = img
 }
 
+// encode is our main function for
+// base64 encoding a passed []byte
 func encode(bin []byte) []byte {
 	e64 := base64.StdEncoding
 
@@ -65,11 +67,12 @@ func get(url string) ([]byte, string) {
 // DEPRECATED
 // Begin a NewImage to fetch
 // TODO: Deprecate NewImage
-func NewImage(url string) string {
+func NewImage(url string) (out string) {
 	image, mime := get(cleanUrl(url))
 	enc := encode(image)
 
-	return make(enc, mime)
+	out = format(enc, mime)
+	return
 }
 
 // FromRemote is a better named function that
@@ -86,14 +89,14 @@ func FromBuffer(buf bytes.Buffer) string {
 	enc := encode(buf.Bytes())
 	mime := http.DetectContentType(buf.Bytes())
 
-	return make(enc, mime)
+	return format(enc, mime)
 }
 
 // FromLocal reads a local file and returns
 // the base64 encoded version.
 func FromLocal(fname string) string {
 	var b bytes.Buffer
-	finfo, err := os.Stat(fname)
+	_, err := os.Stat(fname)
 	if err != nil {
 		if os.IsNotExist(err) {
 			panic("File does not exist")
@@ -114,9 +117,9 @@ func FromLocal(fname string) string {
 	return FromBuffer(b)
 }
 
-// make is an abstraction of the mime switch to create the
+// format is an abstraction of the mime switch to create the
 // acceptable base64 string needed for browsers.
-func make(enc []byte, mime string) string {
+func format(enc []byte, mime string) string {
 	switch mime {
 	case "image/gif", "image/jpeg", "image/pjpeg", "image/png", "image/tiff":
 		return fmt.Sprintf("data:%s;base64,%s", mime, enc)
@@ -126,6 +129,7 @@ func make(enc []byte, mime string) string {
 	return fmt.Sprintf("data:image/png;base64,%s", enc)
 }
 
+// cleanUrl converts whitespace in urls to %20
 func cleanUrl(s string) string {
 	return strings.Replace(s, " ", "%20", -1)
 }
